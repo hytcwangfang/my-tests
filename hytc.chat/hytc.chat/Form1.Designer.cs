6b6664d4c125f46bab93b6f75bdbc08fd606f13e
hytcwangfang <2445923320@qq.com>
2014-08-25 16:39:22 +0800

    2014.8.25.1
    局域网公共聊天室

---
+++ hytc.chat/hytc.chat/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace hytc.chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ip = new System.Windows.Forms.TextBox();
+            this.txt_history = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.txt_send = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_ip
+            // 
+            this.txt_ip.Location = new System.Drawing.Point(12, 12);
+            this.txt_ip.Name = "txt_ip";
+            this.txt_ip.Size = new System.Drawing.Size(292, 21);
+            this.txt_ip.TabIndex = 0;
+            this.txt_ip.Text = "255.255.255.255";
+            // 
+            // txt_history
+            // 
+            this.txt_history.Location = new System.Drawing.Point(12, 39);
+            this.txt_history.Multiline = true;
+            this.txt_history.Name = "txt_history";
+            this.txt_history.Size = new System.Drawing.Size(322, 310);
+            this.txt_history.TabIndex = 1;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(271, 355);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(63, 23);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // txt_send
+            // 
+            this.txt_send.Location = new System.Drawing.Point(12, 357);
+            this.txt_send.Name = "txt_send";
+            this.txt_send.Size = new System.Drawing.Size(253, 21);
+            this.txt_send.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 386);
+            this.Controls.Add(this.txt_send);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_history);
+            this.Controls.Add(this.txt_ip);
+            this.Name = "Form1";
+            this.Text = "chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ip;
+        private System.Windows.Forms.TextBox txt_history;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txt_send;
+    }
+}
+
---
+++ hytc.chat/hytc.chat/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace hytc.chat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            //以字节发送，接受，阅读前转换为string
+            UdpClient udp = new UdpClient();
+            string ip = this.txt_ip.Text;
+            //int length = this.txt_send.MaxLength;
+            string content = "PUBLIC|" + this.txt_send.Text + "|王芳|";
+            byte[] bytes = Encoding.Default.GetBytes(content);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            udp.Send(bytes, bytes.Length, iep);
+        }
+        private void ReceiveData()
+        {
+            UdpClient uc = new UdpClient(9527);
+            while (true)
+            {
+                IPEndPoint iepclient = new IPEndPoint(IPAddress.Any,0);
+                //以字节发送，接受，阅读前转换为string
+                byte[] receivestring = uc.Receive(ref iepclient);
+                string bytes = Encoding.Default.GetString(receivestring);
+                string[] split = bytes.Split('|');
+                if (split[0] == "PUBLIC")
+                {
+                    this.txt_history.Text += split[2] + "说:" + split[1] + "\r\n";
+                }
+                else if(split[0] == "INROOM"){
+                    this.txt_history.Text += split[2] + "上线" + "\r\n";
+                }
+                //else if(split[0] == "OUTROOM"){
+                //    this.txt_history.Text += split[2] + "下线" + "\r\n";
+                //}
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //允许跨线程调用
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            Thread receithread = new Thread(new ThreadStart(ReceiveData));
+            receithread.IsBackground = true;
+            receithread.Start();
+
+            //上线通知
+            UdpClient udp = new UdpClient();
+            string ip = this.txt_ip.Text;
+            //int length = this.txt_send.MaxLength;
+            string content = "INROOM|"  + "|王芳|";
+            byte[] bytes = Encoding.Default.GetBytes(content);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            udp.Send(bytes, bytes.Length, iep);
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+
+            //下线通知
+            //UdpClient udp = new UdpClient();
+            //string ip = this.txt_ip.Text;
+            ////int length = this.txt_send.MaxLength;
+            //string content = "OUTROOM|" + "|王芳|";
+            //byte[] bytes = Encoding.Default.GetBytes(content);
+            //IPEndPoint iep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            //udp.Send(bytes, bytes.Length, iep);
+            
+        }
+
+
+
+       
+
+       
+    }
+}
